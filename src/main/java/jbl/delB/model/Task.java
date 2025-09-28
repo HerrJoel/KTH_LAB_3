7ede2d529ac6c86df7781f54253f2dc99e3cbfd8
erikl <elagerg@kth.se>
2025-09-28 12:59:41 +0200

    Fixat Task, är dock osäker på om compareTo är fungerar korrekt

--- src/main/java/jbl/delB/model/Task.java
+++ src/main/java/jbl/delB/model/Task.java
@@ -1,4 +1,63 @@
 package jbl.delB.model;
 
-public class Task {
+import jbl.delB.enums.TaskPrio;
+import jbl.delB.enums.TaskState;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Task implements Comparable<Task>, Serializable {
+
+    public String description;
+    public int id;
+    public String takenBy;
+    public TaskState state;
+    public LocalDate lastUpdate;
+    public TaskPrio prio;
+
+
+    Task(String descr, TaskPrio prio, int id){
+        this.description=descr;
+        this.prio=prio;
+        this.id=id;
+
+        this.lastUpdate= LocalDate.now();
+        this.state=TaskState.TO_DO;
+    }
+
+    public void setTakenBy(String takenBy) {
+        if (this.takenBy != null) {
+            throw new IllegalStateException("Task has been taken by: " + this.takenBy);
+        }
+
+        this.takenBy = takenBy;
+        this.lastUpdate = LocalDate.now();
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+        this.lastUpdate = LocalDate.now();
+    }
+    public void setPrio(TaskPrio prio) {
+        this.prio = prio;
+        this.lastUpdate = LocalDate.now();
+    }
+
+
+    @Override
+    public int compareTo(Task o) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", id=" + id +
+                ", takenBy='" + takenBy + '\'' +
+                ", state=" + state +
+                ", lastUpdate=" + lastUpdate +
+                ", prio=" + prio +
+                '}';
+    }
 }
